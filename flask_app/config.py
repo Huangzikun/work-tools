5b47831dc4bf0c93e1b33070938cacf6750c2de7
huangzikun <381560728@qq.com>
2025-06-29 16:43:51 +0800

    cp and sign

--- flask_app/config.py
+++ flask_app/config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     # 数据库 URI（格式：数据库类型+驱动://用户:密码@主机:端口/数据库名）
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://glc_work_tools:Glc11223344@101.37.164.15:3306/glc_work_tools'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 关闭修改追踪（减少性能消耗）
